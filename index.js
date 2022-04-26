6afcbab0a85a92b861dfa4cda5f4e50db17af61d
Charanpreet Chall <charanpreetchall@gmail.com>
2022-04-26 16:28:33 -0400

    Edited JSON file for image paths

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ;
 
 //Middlewares
 app.use(express.static('public'))
