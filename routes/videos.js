a8c6699e7e15619d4b077b3dd1ec35631c2027e4
Charanpreet Chall <charanpreet.chall@rossvideo.com>
2023-05-18 10:43:27 -0400

    Updated node modules

--- routes/videos.js
+++ routes/videos.js
@@ -5,69 +5,68 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 const getVideos = () => {
-    const videos = fs.readFileSync('./data/videos.json');
-    return JSON.parse(videos);
-}
+  const videos = fs.readFileSync("./data/videos.json");
+  return JSON.parse(videos);
+};
 
 const saveVideos = (updatedVideos) => {
-    fs.writeFileSync('./data/videos.json', JSON.stringify(updatedVideos))
-}
+  fs.writeFileSync("./data/videos.json", JSON.stringify(updatedVideos));
+};
 
+router
+  .route("/")
+  .get((_req, res) => {
+    let formattedVideos = getVideos().map((video) => {
+      return {
+        channel: video.channel,
+        id: video.id,
+        image: video.image,
+        title: video.title,
+      };
+    });
 
-router.route("/")
-    .get((_req, res) => {
+    res.status(200).json(formattedVideos);
+  })
 
-        let formattedVideos = getVideos()
-            .map(video => {
-                return {
-                    "channel": video.channel,
-                    "id": video.id,
-                    "image": video.image,
-                    "title": video.title,
-                }
-            })
+  .post((req, res) => {
+    const newVideo = {
+      id: uuidv4(),
+      title: req.body.title,
+      channel: "Ted Lasso",
+      image: req.body.image,
+      description: req.body.description,
+      views: "0",
+      likes: "0",
+      duration: "5:00",
+      video: "",
+      timestamp: Date.now(),
+      comments: [],
+    };
 
-        res.status(200).json(formattedVideos)
-    })
+    let updatedVideos = getVideos();
+    updatedVideos.push(newVideo);
 
-    .post((req, res) => {
-        const newVideo = {
-            id: uuidv4(),
-            title: req.body.title,
-            channel: 'Ted Lasso',
-            image: req.body.image,
-            description: req.body.description,
-            views: "0",
-            likes: "0",
-            duration: "5:00",
-            video: "",
-            timestamp: Date.now(),
-            comments: [],
-        }
+    saveVideos(updatedVideos);
 
-        let updatedVideos = getVideos();
-        updatedVideos.push(newVideo);
+    res.status(201).send({
+      id: newVideo.id,
+      status: "successful",
+    });
+  });
 
-        saveVideos(updatedVideos);
+router.get("/:id", (req, res) => {
+  const individualVideo = getVideos().find(
+    (video) => video.id === req.params.id
+  );
 
-        res.status(201).send({
-            "id": newVideo.id,
-            "status": "successful"
-        })
-    })
+  if (!individualVideo) {
+    res.status(404).json({
+      message: "Video not found",
+    });
+    return;
+  }
 
-
-router.get('/:id', (req, res) => {
-    const individualVideo = getVideos().find(video => video.id === req.params.id)
-
-    if (!individualVideo) {
-        res.status(404).json({
-            message: ("Video not found")
-        })
-        return;
-    }
-
-    res.status(201).json(individualVideo)
-})
+  res.status(201).json(individualVideo);
+});
 
 module.exports = router;
